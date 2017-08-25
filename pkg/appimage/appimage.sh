ea72653e28981ef35661d742c0bd4f61a7108f14
Akira Ohgaki <akiraohgaki@gmail.com>
2017-08-25 11:42:11 +0900

    Update appimage.sh

--- pkg/appimage/appimage.sh
+++ pkg/appimage/appimage.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+PKGNAME='ocs-manager'
+
 sh scripts/import.sh
 qmake PREFIX="/usr"
 make
-make INSTALL_ROOT="ocs-manager.AppDir" install
+make INSTALL_ROOT="${PKGNAME}.AppDir" install
 
 curl -L -o linuxdeployqt.AppImage "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod 755 linuxdeployqt.AppImage
 ./linuxdeployqt.AppImage --appimage-extract
 
-./squashfs-root/AppRun ocs-manager.AppDir/usr/share/applications/ocs-manager.desktop -bundle-non-qt-libs -no-translations
-./squashfs-root/AppRun ocs-manager.AppDir/usr/share/applications/ocs-manager.desktop -appimage
+./squashfs-root/AppRun ${PKGNAME}.AppDir/usr/share/applications/${PKGNAME}.desktop -bundle-non-qt-libs -no-translations
+./squashfs-root/AppRun ${PKGNAME}.AppDir/usr/share/applications/${PKGNAME}.desktop -appimage
