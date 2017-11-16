cd35eecff973c2b372d1f6ed4bf5755709488165
Akira Ohgaki <akiraohgaki@gmail.com>
2017-11-16 23:23:00 +0900

    Import AppImageUpdate

--- scripts/import.sh
+++ scripts/import.sh
@@ -5,3 +5,8 @@ PROJDIR="$(cd "$(dirname "${0}")/../" && pwd)"
 if [ ! -d "${PROJDIR}/lib/qtlib" ]; then
     git clone https://github.com/akiraohgaki/qtlib.git -b release-0.2.1 --single-branch --depth=1 "${PROJDIR}/lib/qtlib"
 fi
+
+if [ ! -d "${PROJDIR}/lib/AppImageUpdate" ]; then
+    git clone https://github.com/AppImage/AppImageUpdate.git -b continuous --single-branch --depth=1 "${PROJDIR}/lib/AppImageUpdate"
+    git -C "${PROJDIR}/lib/AppImageUpdate" submodule update --init --recursive --depth=1
+fi
