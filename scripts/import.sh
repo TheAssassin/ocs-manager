f2fa0056cbadccf902f45dbd93a9c139924096b1
Akira Ohgaki <akiraohgaki@gmail.com>
2017-11-17 01:11:16 +0900

    Prebuild libappimageupdate

--- scripts/import.sh
+++ scripts/import.sh
@@ -10,3 +10,10 @@ if [ ! -d "${PROJDIR}/lib/AppImageUpdate" ]; then
     git clone https://github.com/AppImage/AppImageUpdate.git -b continuous --single-branch --depth=1 "${PROJDIR}/lib/AppImageUpdate"
     git -C "${PROJDIR}/lib/AppImageUpdate" submodule update --init --recursive --depth=1
 fi
+
+if [ ! -d "${PROJDIR}/lib/AppImageUpdate-prebuilt" ]; then
+    mkdir "${PROJDIR}/lib/AppImageUpdate-prebuilt"
+    cd "${PROJDIR}/lib/AppImageUpdate-prebuilt"
+    cmake "${PROJDIR}/lib/AppImageUpdate" -DUSE_SYSTEM_CURL=OFF -DBUILD_CPR_TESTS=OFF -DCMAKE_BUILD_TYPE=RelWithDebInfo
+    make libappimageupdate
+fi
