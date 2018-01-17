cdb1c5f76f75e5e5dee7f8ea793cc7d971aeab1f
Akira Ohgaki <akiraohgaki@gmail.com>
2018-01-18 01:18:14 +0900

    Update build-docker.sh

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -35,11 +35,11 @@ build_flatpak() {
 
 build_appimage() {
     # docker-image: ubuntu:14.04
+
     apt-get update -qq
     apt-get -y install curl git
-    apt-get -y install build-essential cmake
-    #apt-get -y install qt5-default libqt5websockets5-dev
-    #apt-get -y install libssl-dev libcurl4-gnutls-dev libxpm-dev
+    #apt-get -y install build-essential qt5-default libqt5websockets5-dev
+    #apt-get -y install cmake libssl-dev libcurl4-gnutls-dev libxpm-dev
     apt-get -y install libssl1.0.0 zlib1g
 
     add-apt-repository -y ppa:beineri/opt-qt593-trusty
@@ -47,9 +47,10 @@ build_appimage() {
     curl -fsSL https://download.opensuse.org/repositories/home:TheAssassin:AppImageLibraries/xUbuntu_14.04/Release.key | apt-key add -
     apt-get update -qq
 
-    apt-get -y install qt59base qt59websockets
+    apt-get -y install build-essential qt59base qt59websockets
     source /opt/qt59/bin/qt59-env.sh
 
+    curl -fsSL https://cmake.org/files/v3.10/cmake-3.10.0-rc5-Linux-x86_64.tar.gz | tar -xz --strip-components=1 -C /
     apt-get -y install libssl-dev libcurl3 libcurl3-gnutls libxpm-dev
 
     useradd -m ${PKGUSER}
