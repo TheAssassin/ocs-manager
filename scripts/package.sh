f63d62b4492dfb0cfcde2e14b9d7c32e63d84200
Akira Ohgaki <akiraohgaki@gmail.com>
2018-02-13 01:05:41 +0900

    Update package.sh

--- scripts/package.sh
+++ scripts/package.sh
@@ -12,20 +12,18 @@ BUILDDIR="${PROJDIR}/build_${PKGNAME}"
 
 ci_appimage() { # docker-image: ubuntu:14.04
     apt update -qq
-    apt -y install software-properties-common
     apt -y install curl git
     #apt -y install build-essential qt5-default libqt5websockets5-dev
-    #apt -y install cmake libssl-dev libcurl4-gnutls-dev libxpm-dev
-    apt -y install libssl1.0.0 zlib1g
-    apt -y install unzip p7zip
+    #apt -y install cmake libssl-dev libcurl3 libcurl3-gnutls libcurl4-gnutls-dev libxpm-dev
+    apt -y install libssl1.0.0 zlib1g unzip p7zip
 
+    apt -y install software-properties-common
     add-apt-repository -y ppa:beineri/opt-qt593-trusty
     echo 'deb http://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_14.04/ /' > /etc/apt/sources.list.d/curl-httponly.list
     curl -fsSL https://download.opensuse.org/repositories/home:TheAssassin:AppImageLibraries/xUbuntu_14.04/Release.key | apt-key add -
     apt update -qq
 
-    apt -y install build-essential libgl1-mesa-dev qt59base qt59websockets
-
+    apt -y install build-essential libfontconfig1 mesa-common-dev libglu1-mesa-dev qt59base qt59websockets
     curl -fsSL https://cmake.org/files/v3.10/cmake-3.10.0-rc5-Linux-x86_64.tar.gz | tar -xz --strip-components=1 -C /
     apt -y install libssl-dev libcurl3 libcurl3-gnutls libcurl4-gnutls-dev libxpm-dev
 
