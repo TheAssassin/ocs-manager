c2d2dd3babb5bcb6593c6c781c9e69da8dc45346
Akira Ohgaki <akiraohgaki@gmail.com>
2018-02-16 18:12:15 +0900

    0.5.5

--- pkg/appimage/appimage.sh
+++ pkg/appimage/appimage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PKGNAME='ocs-manager'
-PKGVER='0.5.4'
+PKGVER='0.5.5'
 PKGREL='1'
 
 curl -fsSL -o linuxdeployqt.AppImage https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
